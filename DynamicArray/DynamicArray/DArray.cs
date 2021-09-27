ab2c03dbc5b1ac005436d14e768a51dd889b532f
VaishnavNimkar <vaishnavnimkar8@gmail.com>
2021-09-28 01:04:15 +0530

    Dynamic Array with Generics

---
+++ DynamicArray/DynamicArray/DArray.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DynamicArray
+{
+	public class DArray<T>
+	{
+        private T[] buffer = new T[5];
+        private int itemCount;
+        public T this[int index]
+        {
+            set
+            {
+                if (index >= buffer.Length)
+                {
+                    Array.Resize(ref buffer, index + 1);
+                }
+
+                buffer[index] = value;
+                itemCount++;
+            }
+            get
+            {
+                return buffer[index];
+            }
+        }
+
+        public void PrintArray()
+        {
+            Console.WriteLine(String.Join(" ", buffer));
+        }
+    }
+}
---
+++ DynamicArray/DynamicArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicArray
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+            DArray<int> intArray = new DArray<int>();
+            intArray[0] = 10;
+            intArray[1] = 20;
+            intArray[2] = 30;
+            intArray[3] = 40;
+            intArray[5] = 50;
+            intArray.PrintArray();
+            DArray<float> floatArray = new DArray<float>();
+            floatArray[0] = 10.5F;
+            floatArray[2] = 12.5F;
+            floatArray[3] = 13.5F;
+            floatArray[4] = 14.5F;
+            floatArray.PrintArray();
+
+            Console.ReadKey();
+        }
+	}
+}
