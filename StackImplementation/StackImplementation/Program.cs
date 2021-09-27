9b10c33100908e442ec8853b05520070133749f3
VaishnavNimkar <vaishnavnimkar8@gmail.com>
2021-09-28 00:57:03 +0530

    Stack Implementation solution

---
+++ StackImplementation/StackImplementation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StackImplementation
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Stack stk = new Stack();
+			stk.Push(10);
+			stk.Push(20);
+			stk.Push(30);
+			stk.Push(40);
+			Console.WriteLine("Popped item = " + stk.Pop());
+			stk.Print();
+			stk.Clear();
+			stk.Print();
+			Console.ReadKey();
+		}
+	}
+}
---
+++ StackImplementation/StackImplementation/Stack.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace StackImplementation
+{
+	public class Stack
+	{
+		int top = -1;
+		int[] arr = new int[5];
+
+		public void Push(int num)
+		{
+			if (top < 4)
+				arr[++top] = num;
+			else
+				Console.WriteLine("Stack Overflow");
+		}
+
+		public int Pop()
+		{
+			if (top < 0)
+			{
+				Console.WriteLine("Stack Underflow");
+				return -1;
+			}
+
+
+			int temp = 0;
+			temp = arr[top--];
+			arr[top+1] = 0;
+			return temp;
+		}
+
+		public void Print()
+		{
+			Console.WriteLine(String.Join(" ", arr));
+		}
+
+		public void Clear()
+		{
+			arr = new int[5];
+		}
+	}
+}
